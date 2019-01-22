0a15ea84c73624ae596f9c622f1c43c066251c63
Manosfrias <a.gilamor@gmail.com>
2019-01-22 15:55:18 +0100

    boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,44 @@
-const glob = require('glob');
-let entry = __dirname + '/src/js/script.js';
-let outputPath = __dirname + '/dist/';
-let devtool = 'source-map';;
+
+const path = require('path').resolve(__dirname, 'dist');
+const main = './src/js/index.js';
+const filename = '[name].[chunkhash].js';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: entry,
+  entry: { main: main },
   output: {
-    path: outputPath,
+    path: path,
+    filename: filename
   },
-  devtool: devtool,
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: ['/node_modules'],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env'],
-            }
-          }
-        ]
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[c|a]ss$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
